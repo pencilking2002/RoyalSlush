3c19819972cc19cc7f3b68e6406b0f836888dfe6
Garbanzo\Yiyi-Guest <pencilking2002@gmail.com>
2017-06-19 22:24:05 -0400

    Created stamd in Blender and Smoothie Items

---
+++ RoyalSlush/FruitWarriorMR-master/Assets/BlenderController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BlenderController : MonoBehaviour {
+
+	public static BlenderController Instance;
+
+	public Action onAddSmoothieItem;
+
+	/// <summary>
+	/// Particles that get emitted when a food item is dropped into the blender
+	/// </summary>
+	public ParticleSystem dropParticles;
+
+	private Rigidbody rb;
+	//private List<string> foodTagList;
+
+	// List that contain all the items in a smoothie
+	public List<SmoothieItem> smoothiItems = new List<SmoothieItem> ();
+
+	private void Awake()
+	{
+		InitInstance ();
+		rb = GetComponent<Rigidbody> ();
+	}
+
+
+	/// <summary>
+	/// Play a particle effect when a food item collides with the blender
+	/// </summary>
+	private void PlayDropParticles()
+	{
+		if (dropParticles != null) 
+		{
+			dropParticles.Play ();
+		}
+	}
+
+	/// <summary>
+	/// Adds an item to the blender
+	/// </summary>
+	/// <param name="item">Item.</param>
+	public void AddSmoothieItem(SmoothieItem item)
+	{
+		if (!smoothiItems.Contains (item)) 
+		{
+			smoothiItems.Add (item);
+
+			print ("Item added");
+
+			if (onAddSmoothieItem != null)
+				onAddSmoothieItem ();
+		}
+	}
+
+	private void InitInstance()
+	{
+		if (Instance == null)
+			Instance = this;
+		else
+			Destroy (gameObject);
+	}
+
+}
---
+++ RoyalSlush/FruitWarriorMR-master/Assets/SmoothieItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for identifying what kind of smoothieitem this is and also
+/// adding itself to a smoothie upon collision with the blender
+/// </summary>
+public class SmoothieItem : MonoBehaviour {
+	public SmoothieItemType type;
+
+	/// <summary>
+	/// Add the smoothie item to the blender when it collides
+	/// </summary>
+	/// <param name="other">Other.</param>
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.collider.CompareTag ("Blender")) 
+		{
+			BlenderController.Instance.AddSmoothieItem (this);
+		}	
+	}
+}
+
+public enum SmoothieItemType {
+	Apple,
+	Cucumber,
+}
