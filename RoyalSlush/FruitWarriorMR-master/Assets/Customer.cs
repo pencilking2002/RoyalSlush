aecced30419d1172c9737bc39d1d82c0d524c319
Roman <pencilking2002@gmail.com>
2017-06-20 10:07:42 -0400

    - Made Chalkboard - Added BlenderController

---
+++ RoyalSlush/FruitWarriorMR-master/Assets/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour {
+	public OrderType orderType;
+
+	private void PlaceOrder(OrderType orderType)
+	{
+		this.orderType = orderType;
+	}
+}
+
+public enum OrderType {
+	Sweet,
+	Juicy,
+	Sour,
+	Healthy,
+	Energetic
+}
