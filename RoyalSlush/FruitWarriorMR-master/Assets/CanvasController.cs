b96f09ae408a1c49ca018d876d5274edc9fc2899
Roman <pencilking2002@gmail.com>
2017-06-20 12:57:08 -0400

    blender controller prefab

---
+++ RoyalSlush/FruitWarriorMR-master/Assets/CanvasController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CanvasController : MonoBehaviour {
+
+	public static CanvasController Instance;
+	public Text blackboaxrdText; 
+	private Canvas canvas;
+
+	private void Awake()
+	{
+		InitInstance ();
+		canvas = GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void InitInstance()
+	{
+		if (Instance == null)
+			Instance = this;
+		else
+			Destroy (gameObject);
+	}
+
+	public void ReplaceText(string text)
+	{
+		this.blackboaxrdText.text = text;	
+	}
+}
--- RoyalSlush/FruitWarriorMR-master/Assets/Customer.cs
+++ RoyalSlush/FruitWarriorMR-master/Assets/Customer.cs
@@ -1,20 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+public enum SmoothieType {
+	Healthy,
+	Sweet,
+	Sour
+}
+
+public enum SmoothieModifier {
+	Death,
+	Luck,
+	TheFunk,
+	PunchInTheFace
+}
+
 
 public class Customer : MonoBehaviour {
-	public OrderType orderType;
+	public SmoothieOrder order;
 
-	private void PlaceOrder(OrderType orderType)
+	private void Awake()
 	{
-		this.orderType = orderType;
+		order = new SmoothieOrder();
+	}
+
+
+	public static Dictionary<SmoothieType,string> Types = new Dictionary<SmoothieType, string>()
+	{
+		{ SmoothieType.Healthy, "healthy" },
+		{ SmoothieType.Sweet, "sweet" },
+		{ SmoothieType.Sour, "sour" }
+	};
+
+	public static Dictionary<SmoothieModifier,string> Modifiers = new Dictionary<SmoothieModifier, string>()
+	{
+		{ SmoothieModifier.Death, "death" },
+		{ SmoothieModifier.Luck, "luck" },
+		{ SmoothieModifier.TheFunk, "the funk" },
+		{ SmoothieModifier.PunchInTheFace, "punch in the face" }
+	};
+
+	public static List<string> hintList = new List<string>()
+	{
+		{ "with a hint of" },	
+		{ "and a touch of" },	
+		{ "and a little bit of" },	
+		{ "and can you add some" }
+
+	};
+
+	private void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+
+
+			//print("Hello");
+			PlaceOrder();
+		}
+	}
+
+	private void PlaceOrder()
+	{
+		
+		int randNum = UnityEngine.Random.Range(0,4);
+		order.type = Types[ (SmoothieType) randNum];
+
+		Debug.Log(order.type);
+
+		randNum = UnityEngine.Random.Range(0,5);
+		order.modifier = Modifiers[ (SmoothieModifier) randNum];
+
+		randNum = UnityEngine.Random.Range(0,5);
+
+		var customerOrder = "I would like a " + order.type + " smoothie " + hintList[randNum] + " " + order.modifier;
+		CanvasController.Instance.ReplaceText(customerOrder);
 	}
 }
 
-public enum OrderType {
-	Sweet,
-	Juicy,
-	Sour,
-	Healthy,
-	Energetic
+
+public class SmoothieOrder
+{
+	public string type;
+	public string modifier;
 }
+
